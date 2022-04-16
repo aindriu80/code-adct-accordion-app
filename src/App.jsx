6706b4b35b2bd8cfcc881415d54d90298501dbac
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-04-16 11:35:43 +0100

    Iterating over questions with map, display/hide answers with state, react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import { useState } from 'react'
-import './App.css'
+import Question from './Question';
+import data from './data';
+import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <h2>Accordion App</h2>
-    </div>
-  )
+    <main>
+      <div className='container'>
+        <h2>Accordion App</h2>
+
+        <section className='info'>
+          {data.map((question) => {
+            return <Question key={question.id} {...question} />;
+          })}
+        </section>
+      </div>
+    </main>
+  );
 }
 
 export default App
